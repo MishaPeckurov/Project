afb576c780bc6394e02eda82031a5a9e5df54c22
MishaPeckurov <146089071+MishaPeckurov@users.noreply.github.com>
2023-12-06 20:35:16 +0200

    Add files via upload

---
+++ дз14/1111.py
@@ -0,0 +1,7 @@
+class qpo:
+    nama = 'Misha'
+    aga = '8'
+    def printt(self):
+    print(self.nama,self.aga)
+a = qpo()
+a.printt()
---
+++ дз14/3tert.py
@@ -0,0 +1,6 @@
+class pqwe:
+    a = 'Misha'
+    b = 'Maxim'
+human = pqwe()
+print(human.a)
+print(human.b)
