8456a3482f54088d77bdbb0feab0547879500bd6
MishaPeckurov <146089071+MishaPeckurov@users.noreply.github.com>
2023-12-16 14:22:48 +0200

    Add files via upload

---
+++ дз15/1111.py
@@ -0,0 +1,9 @@
+class mun:
+    def __init__(self,num1,num2,num3):
+        self.num1 = num1
+        self.num2 = num2
+        self.num3 = num3
+    def mihdh(self):
+        print((self.num1 + self.num2 + self.num3)/3)
+i = mun(1,5,10)
+i.mihdh()
---
+++ дз15/3tert.py
@@ -0,0 +1,10 @@
+class less:
+    __pole1 = 'Misha'
+    __pole2 = 'MaximYeah you will watch'
+    __pole3 = 8
+    def __printName(self):
+        print(type(self.__pole1),(self.__pole2),(self.__pole3))
+    def pr(self):
+        self.__printName()
+human = less()
+human.pr()
