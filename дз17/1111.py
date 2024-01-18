ee93ca4d81af22244fb88e310da0bce6544d7190
MishaPeckurov <146089071+MishaPeckurov@users.noreply.github.com>
2024-01-18 21:34:19 +0200

    Add files via upload

---
+++ дз17/1111.py
@@ -0,0 +1,11 @@
+class conver:
+    
+    __a = 12
+    __b = 44
+
+    def p(self):
+        print(self.__a,self.__b)
+        
+
+c = conver()
+c.p()
---
+++ дз17/3tert.py
@@ -0,0 +1,10 @@
+class conver:
+    
+    
+
+    def __call__(self,a):
+        return len(a)
+        
+
+c = conver()
+print(c('awd'))
